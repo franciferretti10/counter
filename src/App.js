06cea3d706de4f48e50e1c4fa9376c1139b153a9
Rocco gualtieri <>
2018-10-18 16:03:36 +0200

    First Commit counter web app

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class App extends React.Component{
+  constructor(){
+   super();
+      this.state = {
+          counter: 0
+      }
+    }
+
+    renderButtonComponent = (text, callback) => {
+      return(
+        <button onClick = { callback }>{text}</button>
+      );
+    }
+
+  
+  render(){
+    return(
+      <div>
+      <h1>Counter: {this.state.counter} </h1>
+        { this.renderButtonComponent('Increment',() => this.setState({ counter: this.state.counter +1})) }
+        { this.renderButtonComponent('Decrement',() => this.setState( {counter: this.state.counter -1})) } 
+      </div>
+    );
+
+  }
+  
+}
+
+export default App;
